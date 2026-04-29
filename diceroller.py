8cc11c731813cff8107a1ca9a80d1e85bd9835e9
BHopper9970 <freobenny@gmail.com>
2026-04-29 12:57:13 +0800

    added icon improved individual roll result renderting (no longer has leftovers from longer numbers) Issue: extra rows still stay after number of dices rolled decreases

--- diceroller.py
+++ diceroller.py
@@ -11,6 +11,7 @@ Version 1.0
 import random
 from tkinter import *
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 def roll_dice():
     total = 0
@@ -23,27 +24,33 @@ def roll_dice():
         roll = random.randint(1, dice_sides) # get roll between 1 and dicesides
         total = total + roll # add roll to total
         print(n + 1, 'roll: ', roll) # print current roll
+        if roll <= 9:
+            roll = '  ' + str(roll)
         results.append(roll)
     output.config(text=total)
-
+    
     itert = 0
     for r in results:
         print(itert)
         globals()[f'var_{itert}'] = ttk.Label(root, text=results[itert])
         exec(f'var_{itert}.grid(row=5 + {itert})')
         itert = itert + 1
-    
-    
+    return
 
 root = Tk()
 results = []
+root.title('Dice Roller')
+image = Image.open("d20.png")
+ico = ImageTk.PhotoImage(image)
+root.iconphoto(False, ico)
 ttk.Label(root, text="How many sides?").grid(row=0)
 ttk.Label(root, text="How many dice?").grid(row=1)
 ttk.Label(root, text="Total:").grid(row=4)
 
 sides = ttk.Entry(root)
 count = ttk.Entry(root)
-
+count.insert(0, "1")
+sides.focus()
 sides.grid(row=0, column=1)
 count.grid(row=1, column=1)
 
